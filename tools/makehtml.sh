0ae06b6a162c7c8f75010adf8033996085abb123
Jordan Wiens <jordan@psifertex.com>
2015-11-01 16:31:06 -0500

    fix html generator

--- tools/makehtml.sh
+++ tools/makehtml.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #This is an awful hack that will only work on the same size ROM. Fix later. 
-(cat tools/PwnAdventureZ.html.1; xxd -i PwnAdventureZ.nes|tail -n+2|head -n 10924; cat tools/PwnAdventureZ.html.2) > PwnAdventureZ.html
+#(cat tools/PwnAdventureZ.html.1; xxd -i PwnAdventureZ.nes|tail -n+2|head -n 10924; cat tools/PwnAdventureZ.html.2) > PwnAdventureZ.html
+(cat tools/PwnAdventureZ.html.1; xxd -i PwnAdventureZ.nes|tail -n+2|grep -Ev '(^}|unsigned)'; cat tools/PwnAdventureZ.html.2) > PwnAdventureZ.html
