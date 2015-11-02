27f42ddb45a823be282d8b51668fce372aef8763
Jordan Wiens <jordan@psifertex.com>
2015-11-02 04:04:38 -0500

    leaky flags

--- flag/flagemu.asm
+++ flag/flagemu.asm
@@ -7,11 +7,13 @@ VAR flag_strings
 	.byte "XXXXXXXXXXXXXXXXXXXXXXXXXXX", 0
 	.byte "PWN THIS ON THE REAL SYSTEM ", 0
 	.byte "TO GET THE ACTUAL FLAG ", 0
+	.byte 0
 
 VAR memory_leak_flag
 	.byte "LEAK THIS FLAG FOR MAXIMUM POINTS.", 0
 	.byte "THE FLAG IS:", 0
 	.byte "XXXXXXXXXXXXXXXXXXXXXXXXXXX", 0
+	.byte 0
 
 
 .segment "UI"
@@ -21,9 +23,11 @@ VAR blocky_flag_text
 	.byte "XXXXXXXXXXXXXXXXXXXXXX", 0
 	.byte "PWN ON THE REAL SYSTEM", 0
 	.byte "TO GET THE ACTUAL FLAG", 0
+	.byte 0
 
 VAR boarded_house_flag_text
 	.byte "THE FLAG IS:", 0
 	.byte "XXXXXXXXXXXXXXXXXXXXXX", 0
 	.byte "PWN ON THE REAL SYSTEM", 0
 	.byte "TO GET THE ACTUAL FLAG", 0
+	.byte 0
--- flag/flagreal.asm
+++ flag/flagreal.asm
@@ -7,11 +7,13 @@ VAR flag_strings
 	.byte "WASEASIERTHANSMWCREDITSWARP", 0
 	.byte "CONGRATULATIONS ON DEFEATING", 0
 	.byte "A REAL ZOMBIE, THE 6502", 0
+	.byte 0
 
 VAR memory_leak_flag
 	.byte "LEAK THIS FLAG FOR MAXIMUM POINTS.", 0
 	.byte "THE FLAG IS:", 0
 	.byte "ZOMBIESHELLCODE4DEADSYSTEMZ", 0
+	.byte 0
 
 
 .segment "UI"
@@ -21,9 +23,11 @@ VAR blocky_flag_text
 	.byte "LINKNEVERHADITTHISHARD", 0
 	.byte "                      ", 0
 	.byte "                      ", 0
+	.byte 0
 
 VAR boarded_house_flag_text
 	.byte "THE FLAG IS:", 0
 	.byte "KNOCKEDITOUTOFTHEPARK ", 0
 	.byte "                      ", 0
 	.byte "                      ", 0
+	.byte 0
